58684ebb53d6d41b0182495bebaa56c86fd097b5
Daniel Sosa <jdsosa@gmail.com>
2019-01-15 16:38:03 +0100

    Add custom tooltips to partners section

--- frontend/web/js/custom.js
+++ frontend/web/js/custom.js
@@ -3,6 +3,9 @@ $(document).on('ready', function () {
   // initialization of carousel
   $.HSCore.components.HSCarousel.init('.js-carousel');
 
+  // initialization of popovers
+  $('[data-toggle="popover"]').popover();
+
   // initialization of HSDropdown component
   $.HSCore.components.HSDropdown.init($('[data-dropdown-target]'), {
     afterOpen: function(){
